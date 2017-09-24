88d2e4f07597fee842bfaeaabe43ccb25b7b6c70
Andrew Wan <32243620+andwan01@users.noreply.github.com>
2017-09-24 21:19:04 +0100

    Class changes on .js files

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const User = require("./user");
 var u1 = new User("Andrew", "Wan");
 console.log(u1);
 console.log(u1.fullName());
-
+console.log(u1.getDateCreated());
--- user.js
+++ user.js
@@ -1,15 +1,19 @@
 "use strict";
 
-module.exports = class User{
+var User = class User{
     constructor(firstName,lastName){
         var _firstName = firstName;
         var _lastName = lastName;
+        var _dateCreated = new Date();
 
         this.getFirstName = function(){ return _firstName;}
         this.getLastName = function(){return _lastName;}
+        this.getDateCreated = function() { return _dateCreated; }
     }
 
     fullName(){
         return this.getFirstName().concat(" ",this.getLastName());
     }
 }
+
+module.exports = User;
